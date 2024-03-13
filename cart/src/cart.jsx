fa4313847c4877fd8845cf1035bda75026583df1
nurovic <mertnurovic@gmail.com>
2024-03-13 14:31:17 +0300

    asd

--- cart/src/cart.jsx
+++ cart/src/cart.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function cart() {
   return (
     <div
-    className="w-64 h-72 bg-yellow-200"
+    className="w-64 h-96 bg-yellow-200"
     >
       <div
       style={{background:"red"}}
